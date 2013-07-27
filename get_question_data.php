a5a60ae1cb017516b4e995fba9d382253830fb18
Nathan Sherburn <nathansherburn@gmail.com>
2013-07-27 21:20:35 +1000

    Add student names to CSV data
    Added a line on top of the CSV table to show student usernames above the
    answers

--- get_question_data.php
+++ get_question_data.php
@@ -42,47 +42,31 @@ if(mysql_affected_rows()==0){ // Do a check for no questions
 
 }
 else{
-	// Output data in XML format
-	//header("Content-type: text/xml"); //Declare saving data in XML form
-	/* echo '<?xml version="1.0" encoding="utf-8"?>'; */
-	//echo "<StudentResults>"; //Top root directory
-	//echo "<UnitCode>$unit_code</UnitCode>";
-	/*$student_list_all = array();
-	$resource_student_list = mysql_query("SELECT * FROM student_list");
-	while ($all_student_list = mysql_fetch_array($resource_student_list)){ 
-		$a_name = ($all_student_list["username"]);
-		array_slice($a_name, 0, 20);
-		array_push($student_list_all, $a_name);
-	
-	print_r($student_list_all);
-	}*/
 	$csv_container = array();
+
+	$result_res = mysql_query("SELECT * FROM student_list");
+	$question_row = array();
+	while ($student_list = mysql_fetch_array($result_res)) { // Obtain all lecturer quiz questions
+			$student = ($student_list["username"]);
+			array_push($question_row, $student);
+	}
+	array_push($csv_container, $question_row);
+
 	while ($lecturer_questions = mysql_fetch_array($resource)) { // Obtain all lecturer quiz questions
-		// print_r ($lecturer_questions["id"]); for debugging purposes
 		$q_number = $lecturer_questions["id"]; // the question number (ie q_2 in database)
 		$lec_ques = $lecturer_questions["lec_ques"]; // the worded lecturer question
 		$result_res = mysql_query("SELECT * FROM student_list"); // obtaining the resource from the database
-		//echo "<Question>"; // beginning the xml
-		//echo "<QuestionNum>Qestion $question_number</QuestionNum>";
 		$question_row = array();
 		while ($student_list = mysql_fetch_array($result_res)) { // Obtain all lecturer quiz questions
 			$student = ($student_list["username"]);
 			$result = mysql_query("SELECT * FROM q_$q_number WHERE username = '$student'");
 			$row = mysql_fetch_array($result);
 			$answer = $row["mcq_answer"];
-			// Print the XML
-			//echo "<Result>";
-			//echo "<Student>$student</Student>";
-			//echo "<Answer>$answer</Answer>";
 			array_push($question_row, $answer);
-			//echo "</Result>";
 		}
 		array_push($csv_container, $question_row);
-		//print_r($question_row);
-		//echo "</Question>";
 		$question_number ++;
 	}
-	//echo "</StudentResults>";
 }
 
 $fp = fopen('php://output', 'w');
